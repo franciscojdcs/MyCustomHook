4589b5a978c97d32d9f72bdc8e433d8e8540b8d6
franciscojdcs <franciscojdcs@gmail.com>
2022-12-06 05:55:15 -0500

    Include new functionalities: formValidation and other hook of REACT useEffect & useMemo.

--- useForm/useForm.js
+++ useForm/useForm.js
@@ -1,7 +1,25 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
-export const useForm = (initialForm = {}) => {
+export const useForm = (initialForm = {}, formValidations={}) => {
   const [formState, setFormState] = useState(initialForm);
+  const [formValidation, setformValidation] = useState({});
+
+  useEffect(() => {
+    createValidation();
+  }, [formState])
+
+  useEffect(() => {
+    setFormState(initialForm);
+  }, [initialForm])
+  
+  
+  const isFormValid = useMemo(() => {
+    for (const formfield of Object.keys( formValidation) ){
+      if (formValidation[formfield] !== null ) return false;
+    } 
+    return true;
+
+  }, [formValidation])
 
   const onInputChange = ({ target }) => {
     const { name, value } = target;
@@ -15,10 +33,24 @@ export const useForm = (initialForm = {}) => {
     setFormState(initialForm);
   }
 
+  const createValidation = () => {
+    const formCheckedValues = {}
+    for (const formfield of Object.keys( formValidations) ) {
+      
+      const [fn, errorMsg] = formValidations[formfield];
+      formCheckedValues[`${formfield}Valid`] = fn( formState[ formfield ] ) ? null : errorMsg;
+    }
+    setformValidation( formCheckedValues );
+
+  } 
+
   return {
     ...formState, // para desestructurar el objeto
     formState,
     onInputChange,
-    onResetForm
+    onResetForm,
+    isFormValid,
+    ...formValidation,
+    formValidation
   };
 };
